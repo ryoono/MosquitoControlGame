5847367518e58e8b910e41a1c98f9f04b1ff2372
ryota <ono@pluslab.org>
2024-09-15 16:41:36 +0900

    add TextField

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:src/banana_counter.dart';
 
 void main() {
 
-  // final b = BananaCounter(
-  //   number: 888,
-  // );
+  final controller = TextEditingController();
+
+  final textField = TextField(
+    controller: controller,
+    decoration: const InputDecoration(
+      // 1
+      border: OutlineInputBorder(),
+      // 2
+      labelText: 'あなたの名前',
+      // 3
+      hintText: 'カタカナで入力してください',
+      // 4
+      errorText: '名前が長すぎます',
+    ),
+  );
 
   xxxx(){
-    debugPrint('これから通信を始めます');
-    debugPrint('通信中です');
-    debugPrint('通信が終わりました');
+    debugPrint(controller.text);
   }
+  final button = ElevatedButton(onPressed: xxxx, child: Text('ボタン'),);
 
-  final button = ElevatedButton(
-    onPressed: xxxx,
-    child: Text('押してみて'),
+  final col = Column(
+    children: [
+      textField,
+      button,
+    ],
   );
 
   final a = MaterialApp(
     home: Scaffold(
       body: Center(
-        child: button,
+        child: col,
       ),
     ),
   );
